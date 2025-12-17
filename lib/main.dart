45e00559c8bec9004182082ba1fa9c00b0937e84
e-mandy <andymfrd02@gmail.com>
2025-12-18 00:34:23 +0100

    Mise en place du style de al barre de navigation

--- lib/main.dart
+++ lib/main.dart
@@ -24,14 +24,16 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppBar(),
-      body: SingleChildScrollView(
+      body: Container(
+        color: Colors.green,
         child: Column(
           children: [
             SearchSection(),
+            HotelSection()
           ]
-        )
+        ),
       )
-      );
+    );
   }
 }
 
@@ -72,8 +74,70 @@ class SearchSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue,
-      height: 200
+      color: Colors.grey[200],
+      padding: EdgeInsets.fromLTRB(10, 25, 10, 10),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(child: Container(
+                padding: EdgeInsets.only(left: 20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 5,
+                      offset: Offset(1, 1)
+                    )
+                  ]
+                ),
+
+                height: 50,
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "London",
+                    contentPadding: EdgeInsets.all(10),
+                    border: InputBorder.none
+                  ),
+                ),
+              )),
+              SizedBox(width: 10),
+              Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 5,
+                      offset: Offset(0, 4)
+                    )
+                  ],
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(25)
+                  )
+                ),
+                child: ElevatedButton(
+                  onPressed: null, 
+                  child: Icon(Icons.search),
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    
+                  )
+                )
+              )
+            ],
+          ),
+          SizedBox(height: 50,),
+          Container(
+            color: Colors.blue,
+            height: 100
+          )
+        ],
+      )
     );
   }
 }
@@ -84,7 +148,7 @@ class HotelSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue,
+      color: Colors.white,
       height: 200
     );
   }
